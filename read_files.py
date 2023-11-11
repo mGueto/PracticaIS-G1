d5406da7793950dd55edd38dfb02948b83b4223f
mGueto <m.gueto@udc.es>
2023-11-11 10:47:10 +0100

    add comments

--- read_files.py
+++ read_files.py
@@ -23,19 +23,18 @@ def readSQL(uploaded_file) -> pd.DataFrame:
     path: db file path
        Output: 
     Pandas dataframe """
-    conn, fp = SQLconnect(uploaded_file)
+    fp = Path(str(uuid4())) # get the path
+    fp.write_bytes(uploaded_file.getvalue())
+    conn = sql.connect(str(fp)) # create a connection with the database
     with conn as engine:
         tableName = (pd.read_sql_query("SELECT * FROM sqlite_master WHERE type = 'table'", engine))['name'][0]
         table = pd.read_sql_query(f"SELECT * FROM {tableName}", engine)
+    # close the connection with the database to avoid create files
     conn.close()
     fp.unlink()
     return table
 
-def SQLconnect(db_bytes):
-    fp = Path(str(uuid4()))
-    fp.write_bytes(db_bytes.getvalue())
-    conn = sql.connect(str(fp))
-    return conn, fp
+
     
 
 if __name__ == "__main__":
