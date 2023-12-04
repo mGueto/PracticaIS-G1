eb1fc7b665ec6e0052509b3edfdcccf5df6af566
mGueto <m.gueto@udc.es>
2023-12-04 19:09:35 +0100

    Revert "save model button"
    This reverts commit 0ff6e262d3be07f0d62afc232ce68fec2a602248.

--- interface.py
+++ interface.py
@@ -11,38 +11,17 @@ import leer_archivos as l
 import streamlit as st
 import seleccionar_columnas as s
 import modelos as m
-import joblib
-
-
 
 st.header("Training and prediction of linear regression models")
 st.title("Linear regression tool")
 
-data = l.leer_archivos()
-x , y = s.seleccion_columnas(data)
-
-createModelButton = st.sidebar.button("Crear y visualizar modelo")
-saveModelButton = st.sidebar.button("Guardar modelo")
-
-
-if 'modelCreated' not in st.session_state:
-    st.session_state.modelCreated = False
 
 st.sidebar.header("Options")
 
-
-if createModelButton or st.session_state.modelCreated:
-    model = m.crearModelo(data,x,y)
-    st.session_state.modelCreated = True
-
-
-if saveModelButton:
-    savePath = "data/model.pkl" # save in this path because pop_up_save_file makes a copy of
-    #the file stored in that path
-    joblib.dump(model, savePath)
-    # call file pop_up_save_file.py to open a pop up window to copy model.pkl to a new destination
-    subprocess.Popen(["python", "pop_up_save_file.py"])
-
+data = l.leer_archivos()
+x , y = s.seleccion_columnas(data)
+if st.sidebar.button("Crear y visualizar modelo"):
+    modelo = m.crearModelo(data,x,y)
 
 
 
