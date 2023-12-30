35bbe9f07b7efa63b74d9258fa177f72cb5ed8dc
mGueto <m.gueto@udc.es>
2023-12-30 16:56:30 +0100

    fix crearModelo (no longer plots graphics)

--- regresionMultiple.py
+++ regresionMultiple.py
@@ -14,19 +14,5 @@ def regresionMultipleModel(columnas_X, columna_Y):
     # Entrenar el modelo de regresión
     modelo = CustomModel()
     modelo.fit(X, y)
-    coeficientes = modelo.coef_
-    intercepto = modelo.intercept_
-
-    for i, columna in enumerate(X.columns):
-            predicciones = intercepto + coeficientes[i] * X[columna]
-            # Visualizar los datos y la predicción
-            plt.scatter(X[columna], y, label=f'Datos')
-            plt.plot(X[columna], predicciones, label=f'Predicción ({columna})', color='red')
-            plt.xlabel(columna)
-            plt.ylabel(y.name)  # Suponemos que df_Y tiene una sola columna
-            plt.legend()
-            plt.show()
-            st.pyplot(plt)
-            plt.clf()
-            
+    
     return modelo
--- regresionSimple.py
+++ regresionSimple.py
@@ -7,24 +7,9 @@ from class_customModel import CustomModel
 def regresionSimpleModel(columnaX, columnaY):
     x = columnaX.values
     y = columnaY.values
-    modelo = CustomModelo()
+    modelo = CustomModel()
     modelo.fit(x, y)
-    coeficientes= modelo.coef_
-    intercepto = modelo.intercept_
     
-    # Definir colores en función de la variable dependiente
-    colores = plt.cm.viridis(y / y.max())  # Utiliza el mapa de colores "viridis"
-
-    # Visualizar el modelo de regresión lineal simple con colores diferenciados
-    plt.scatter(x, y, c='blue', s=100, label='Datos')
-    plt.plot(x, modelo.predict(x), color='red', linewidth=2, label='Regresión Lineal')
-    
-    plt.title('Modelo de Regresión Lineal Simple')
-    plt.xlabel(columnaX.columns[0])
-    plt.ylabel(columnaY.name)
-    plt.legend()
-    st.pyplot(plt)
-    st.write('Regresión Lineal', modelo)
     
 
     return modelo
